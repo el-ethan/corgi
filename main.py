85e6d279b2fe1dac37259930e5e9b8c98d744b3b
el-ethan <elethan@gmail.com>
2015-08-16 14:38:42 -0500

    Initial commit of corgi files

---
+++ main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+"""corgi: a utility for quick capture of notes and reminders for org mode"""
+import os
+from kivy.app import App
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+import psutil
+
+sync_file = 'to_sync.txt'
+org_file = 'test_org.org'
+# org_file = '~/Dropbox/org_files/da_guai.org'
+
+
+class Corgi(object):
+	
+	@property
+	def running_emacs(self):
+		if 'emacs' in [psutil.Process(p).name() for p in psutil.pids()]:
+			return True
+		return False
+	
+	@property
+	def org_tasks(self):
+		with open(org_file) as org_f:
+			tasks = org_f.readlines()
+		return tasks
+	
+	@property
+	def sync_tasks(self):
+		with open(sync_file) as sf:
+			tasks = sf.readlines()
+		return tasks
+	
+	@property
+	def confirm_synced(self):
+		overlap = [t for t in self.org_tasks if t in self.sync_tasks]
+		if len(overlap) == len(self.sync_tasks):
+			return True
+		return False
+		# raise Exception
+		
+	def sync_to_org(self):
+		if not self.running_emacs and self.sync_tasks:
+			with open(org_file, 'a') as org_f:
+				for st in self.sync_tasks:
+					org_f.write(st)
+			assert self.confirm_synced
+			with open(sync_file, 'w+') as f:
+				f.write('')
+				assert not f.read()
+		return
+
+
+class CaptureBox(BoxLayout):
+	task_input = ObjectProperty()
+	corgi = Corgi()
+	
+	def on_submit(self):
+		task_target_file = sync_file if self.corgi.running_emacs else org_file
+		with open(task_target_file, 'a') as f:
+			f.write('\n' + '** TODO ' + self.task_input.text)
+		
+		self.corgi.sync_to_org()
+		
+
+class CorgiApp(App):
+	
+	def build(self):
+		Config.set('graphics', 'width', '700')
+		Config.set('graphics', 'height', '50')
+		return CaptureBox()
+
+if __name__ == '__main__':
+	if not os.path.isfile(sync_file):
+		with open(sync_file, 'w') as f:
+			f.write('')
+	CorgiApp().run()
+
---
+++ test_corgi.py
@@ -0,0 +1,18 @@
+from mock import patch, DEFAULT
+from main import Corgi
+
+class TestCogi:
+	
+	def test_confirm_sync(self):
+		with patch.multiple('main.Corgi', org_tasks=DEFAULT, sync_tasks=DEFAULT):
+			corgi = Corgi()
+			corgi.sync_tasks = [1, 2]
+			corgi.org_tasks = [1, 3, 4]
+			assert corgi.confirm_synced is False
+			
+			corgi.sync_tasks = [1, 2]
+			corgi.org_tasks = [1, 2, 3, 4]
+			assert corgi.confirm_synced is True
+	
+	def test_sync_to_org(self):
+		pass
