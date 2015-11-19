39ec26cb82af5590bfd1bbb69a59ea646bc1e824
el-ethan <elethan@gmail.com>
2015-11-18 21:28:25 -0600

    Specify KIVY_HOME and PYTHONPATH

--- corgi.sh
+++ corgi.sh
@@ -1,3 +1,9 @@
+
 #!/bin/bash
+
+KIVYPATH="/home/ethan/Dropbox/development/kivy_fork" 
+
+export PYTHONPATH="$PYTHONPATH:$KIVYPATH/kivy"
+export KIVY_HOME="$KIVYPATH/.kivy"
 
 python ~/Dropbox/development/corgi/corgi.py $1
