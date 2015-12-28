8ab99d1caca06757d1bbbebf39c1bfecf66da99c
el-ethan <elethan@gmail.com>
2015-12-27 22:37:11 -0600

    Add parser module and tests

--- test_corgi.py
+++ test_corgi.py
@@ -1,14 +1,10 @@
-from mock import patch, DEFAULT
-from corgi import Corgi
+from datetime import datetime
+from parser import org_timestamp_to_dt
 
-class TestCogi:
-	
-	def test_confirm_sync(self):
-		assert 0
-	
-	def test_sync_to_org(self):
-		assert 0
-		
-	def test_OrgTask(self):
-		# test_task = OrgTask(task='Do something', sched='', deadline='')
-		assert 0
+
+class TestParser(object):
+
+    def test_org_timestamp_to_dt(self):
+        org_timestamp = '<2013-10-06 Sun 1:18>'
+        expected_dt = datetime(2013, 10, 6, 1, 18)
+        assert org_timestamp_to_dt(org_timestamp) == expected_dt
