0cfeaf0ffd289184e3ef02639f4cb182e62b6a30
el-ethan <elethan@gmail.com>
2016-04-12 22:28:40 -0500

    Refactor corgi.el

--- corgi.el
+++ corgi.el
@@ -1,35 +1,37 @@
 (defvar corgi-org-file-path
-    (shell-command-to-string "python -c \"from config import org_file; print org_file\""))
+    (shell-command-to-string "python -c \"from config import org_file; print org_file\"")
+    "Path to org-file retrieved from python config file")
 
-(defvar corgi-org-file (file-name-nondirectory corgi-org-file-path))
+(defvar corgi-org-file (file-name-nondirectory corgi-org-file-path)
+    "Name of org-file")
 
-(defun sync-to-taskpaper ()
+(defun corgi-sync-to-taskpaper ()
   "Sync org file to taskpaper file for mobile access"
   (when (file-equal-p buffer-file-name corgi-org-file-path)
-    (shell-command "~/corgi.sh taskpapersync")))
+    (shell-command "./corgi.sh taskpapersync")))
 
-(add-hook 'after-save-hook #'sync-to-taskpaper)
+(defun corgi-sync-to-org-command ()
+    (message (shell-command-to-string "./corgi.sh orgsync")))
 
-(defun sync-with-corgi ()
+(defun corgi-sync-to-org-initially ()
   "Sync org file with to_sync.txt from corgi capture and mobile capture"
   (when (window-system nil)
-    (message (shell-command-to-string "./corgi.sh orgsync"))))
+    (corgi-sync-to-org-command)))
 
-(defun es/sync-corgi-tasks ()
+(defun corgi-sync-to-org ()
   "Close org file if open and write new tasks to it"
   (let ((taskfile (get-buffer corgi-org-file)))
     (when taskfile
       (switch-to-buffer taskfile)
       (save-buffer)
       (kill-buffer taskfile))
-    (message (shell-command-to-string "./corgi.sh orgsync"))))
+    (corgi-sync-to-org-command)))
 
-(defun es/org-agenda ()
+(defun corgi-org-agenda ()
   (interactive)
-  (es/sync-corgi-tasks)
+  (corgi-sync-to-org)
   (org-agenda)
 )
 
-;; (add-hook 'org-finalize-agenda-hook #'es/sync-corgi-tasks)
-
-(add-hook 'after-init-hook #'sync-with-corgi)
+(add-hook 'after-save-hook #'corgi-sync-to-taskpaper)
+(add-hook 'after-init-hook #'corgi-sync-to-org-initially)
